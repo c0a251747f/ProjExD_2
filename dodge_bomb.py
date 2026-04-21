d1e200bba89f6cfbd82601d21ba5a438e6436be7
Tsubaki <c0a251747@edu.teu.ac.jp>
2026-04-21 14:25:59 +0900

    爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -13,6 +14,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.surface((20,20))
+    pg.draw.circle(bb_img, (225, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)  
+    bb_rct.centery = random.randint(0, HEIGHT)  
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -22,10 +32,10 @@ def main():
         screen.blit(bg_img, [0, 0])
 
         DELTA = {
-            pg.K_UP: (0 , -5),  # 上
-            pg.K_DOWN: (0 , +5),  # 下
-            pg.K_LEFT: (-5, 0),  # 左
-            pg.K_RIGHT: (+5, 0),  # 右
+            pg.K_UP: (0 , -5), 
+            pg.K_DOWN: (0 , +5),  
+            pg.K_LEFT: (-5, 0), 
+            pg.K_RIGHT: (+5, 0),  
         }
 
         key_lst = pg.key.get_pressed()
@@ -46,6 +56,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
