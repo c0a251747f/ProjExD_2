10d91adcbad6c43c07c1b1a9ad05c4f77fadb141
Tsubaki <c0a251747@edu.teu.ac.jp>
2026-04-21 14:06:20 +0900

    辞書作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,7 +6,6 @@ import pygame as pg
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -20,7 +19,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
+
+        DELTA = {
+            pg.K_UP,(0, -5),
+            pg.K_DOWN,(0, +5),
+            pg.K_LEFT,(-5, 0),
+            pg.K_RIGHT,(+5, 0),
+        }
+        
+        for Key, mv in DELTA.items():
+            if key_lst[Key]:
+                sum_mv[0] += mv[1]
+                sum_mv[1] += mv[0]
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -44,3 +55,4 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
